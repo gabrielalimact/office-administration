c41a6e4217136eea8d99555ff35b4ced7191993d
Gabriela <leticia.lima@nees.ufal.br>
2025-08-22 10:57:49 -0300

    adiciona configuração do ESLint, Prettier e arquivos de ignore para formatação automática

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,46 @@
+import { Button, CloseButton, Dialog, Portal } from "@chakra-ui/react"
+
+type ModalProps = {
+  hasButton?: boolean;
+  buttonText?: string;
+  children?: React.ReactNode;
+  icon?: React.ReactNode;
+  title: string;
+}
+export const Modal = ({
+  hasButton = true,
+  buttonText = "Open Modal",
+  icon,
+  children,
+  title,
+}: ModalProps) => {
+  return (
+    <Dialog.Root size="lg" placement="center" motionPreset="slide-in-bottom">
+      <Dialog.Trigger asChild>
+        {hasButton ? (
+          <Button variant="outline" size="sm">
+            {buttonText}
+          </Button>
+        ) : icon}
+      </Dialog.Trigger>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content
+            padding="6"
+          >
+            <Dialog.Header display="flex" justifyContent="space-between" alignItems="center" mb={4}>
+              <Dialog.Title>{title}</Dialog.Title>
+              <Dialog.CloseTrigger asChild>
+                <CloseButton size="sm" />
+              </Dialog.CloseTrigger>
+            </Dialog.Header>
+            <Dialog.Body>
+              {children}
+            </Dialog.Body>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  )
+}
