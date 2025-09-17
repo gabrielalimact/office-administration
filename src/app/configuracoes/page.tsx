d789bd7a71c859356aae21270c188ab4972f49d3
gabrielalimact <lgcl@ic.ufal.br>
2025-09-17 16:14:20 -0300

    adiciona página de configurações com formulário de edição de usuário

---
+++ src/app/configuracoes/page.tsx
@@ -0,0 +1,100 @@
+'use client';
+import { Box, Button, Field, Fieldset, Flex, Input, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { MdOutlineEdit } from 'react-icons/md';
+import { FaRegSave } from 'react-icons/fa';
+
+type UsuarioData = {
+  id: number;
+  name: string;
+  email: string;
+  birthdate: string;
+  cargo: string;
+};
+const ConfiguracoesPage = () => {
+  const [editMode, setEditMode] = useState(false);
+
+  const [usuario, setUsuario] = useState<UsuarioData>({
+    id: 1,
+    name: 'Louise Oliveira',
+    email: 'louise.oliveira@example.com',
+    birthdate: '1990-01-01',
+    cargo: 'Administrador',
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUsuario((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <Box p={6} bg="#f4f8fb" minH="100vh" margin="0 auto">
+      <Text fontSize="2xl" fontWeight="bold" mb={4}>
+        Configurações do Usuário
+      </Text>
+      <Fieldset.Root minW="full" flex={1}>
+        <Fieldset.Content display="flex" flexDir="column">
+          <Field.Root mt={2}>
+            <Field.Label fontWeight="bold">Nome completo</Field.Label>
+            <Input
+              p="12px"
+              name="name"
+              value={usuario.name}
+              onChange={handleInputChange}
+              disabled={!editMode}
+            />
+          </Field.Root>
+
+          <Field.Root mt={2}>
+            <Field.Label fontWeight="bold">Data de nascimento</Field.Label>
+            <Input
+              p="12px"
+              name="birthdate"
+              type="date"
+              value={usuario.birthdate}
+              onChange={handleInputChange}
+              disabled={!editMode}
+            />
+          </Field.Root>
+
+          <Field.Root mt={2}>
+            <Field.Label fontWeight="bold">E-mail</Field.Label>
+            <Input
+              p="12px"
+              name="email"
+              value={usuario.email}
+              onChange={handleInputChange}
+              disabled={!editMode}
+            />
+          </Field.Root>
+
+          <Field.Root mt={2}>
+            <Field.Label fontWeight="bold">Cargo</Field.Label>
+            <Input
+              p="12px"
+              name="cargo"
+              value={usuario.cargo}
+              onChange={handleInputChange}
+              disabled={!editMode}
+            />
+          </Field.Root>
+        </Fieldset.Content>
+      </Fieldset.Root>
+
+      <Flex justifyContent="flex-end" mt={3}>
+        <Button
+          onClick={() => setEditMode((prev) => !prev)}
+          borderRadius="50px"
+          fontWeight="bold"
+          width="150px"
+          bgColor={editMode ? 'green.500' : 'var(--darkblue)'}
+        >
+          {editMode ? <FaRegSave /> : <MdOutlineEdit />}
+          {editMode ? `Salvar` : `Editar`}
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
+
+export default ConfiguracoesPage;
