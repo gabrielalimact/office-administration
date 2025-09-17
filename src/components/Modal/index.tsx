d33eab265aa3d9a50c7c8ed554befc98a889c583
gabrielalimact <lgcl@ic.ufal.br>
2025-09-17 16:14:58 -0300

    ajusta formatação e estilo do componente Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import { Button, CloseButton, Dialog, Portal } from "@chakra-ui/react"
+import { Button, CloseButton, Dialog, Portal } from '@chakra-ui/react';
 
 type ModalProps = {
   hasButton?: boolean;
@@ -6,41 +6,41 @@ type ModalProps = {
   children?: React.ReactNode;
   icon?: React.ReactNode;
   title: string;
-}
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+};
 export const Modal = ({
   hasButton = true,
-  buttonText = "Open Modal",
+  buttonText = 'Open Modal',
   icon,
   children,
   title,
+  size = 'lg',
 }: ModalProps) => {
   return (
-    <Dialog.Root size="lg" placement="center" motionPreset="slide-in-bottom">
+    <Dialog.Root size={size} placement="center" motionPreset="slide-in-bottom">
       <Dialog.Trigger asChild>
         {hasButton ? (
           <Button variant="outline" size="sm">
             {buttonText}
           </Button>
-        ) : icon}
+        ) : (
+          icon
+        )}
       </Dialog.Trigger>
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
-          <Dialog.Content
-            padding="6"
-          >
+          <Dialog.Content padding="6">
             <Dialog.Header display="flex" justifyContent="space-between" alignItems="center" mb={4}>
               <Dialog.Title>{title}</Dialog.Title>
               <Dialog.CloseTrigger asChild>
                 <CloseButton size="sm" />
               </Dialog.CloseTrigger>
             </Dialog.Header>
-            <Dialog.Body>
-              {children}
-            </Dialog.Body>
+            <Dialog.Body>{children}</Dialog.Body>
           </Dialog.Content>
         </Dialog.Positioner>
       </Portal>
     </Dialog.Root>
-  )
-}
+  );
+};
