0d63acf386816c866a646b15677ac352092d2e28
gabrielalimact <lgcl@ic.ufal.br>
2025-10-02 07:00:36 -0300

    feat: adiciona gerenciamento de tokens de autenticação no login

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,13 @@
 import { api } from './api-service';
+import Cookies from 'js-cookie';
 export async function login(cpf: string, senha: string) {
   const params = new URLSearchParams();
   params.append('cpf', cpf);
   params.append('senha', senha);
 
   const response = await api.post('/auth/login', params);
+  Cookies.set('access_token', response.data.access_token);
+  Cookies.set('refresh_token', response.data.refresh_token);
+
   return response.data;
 }
