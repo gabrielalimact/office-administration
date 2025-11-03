077f863c6ac6bc85d8fcb595b9e22cbaee90c74b
gabrielalimact <lgcl@ic.ufal.br>
2025-11-03 14:59:12 -0300

    feat: adiciona ícone de home e melhora a exibição dos breadcrumbs

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -2,7 +2,8 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useBreadcrumb } from './BreadcrumbContext'
-import { IoChevronForward } from 'react-icons/io5'
+import { IoChevronForward, IoHome } from 'react-icons/io5'
+import { capitalizeFirstLetter } from '../../utils/string'
 
 export default function Breadcrumb() {
   const { breadcrumbs } = useBreadcrumb()
@@ -12,29 +13,28 @@ export default function Breadcrumb() {
   return (
     <Box mb={4} p={3} borderBottom={'1px solid #c2c2c2ff'}>
       <Flex align="center" gap={2} flexWrap="wrap">
+        <IoHome size={16} color="#4A90E2" />
+
         {breadcrumbs.map((item, index) => (
           <Flex key={item.path} align="center" gap={2}>
             {index === breadcrumbs.length - 1 ? (
-              <Flex align="center" gap={2}>
-                {item.icon}
-                <Text color="gray.900" fontWeight="bold">
-                  {item.label}
-                </Text>
-              </Flex>
+              <Text color="gray.600" fontSize="sm">
+                {capitalizeFirstLetter(item.label)}
+              </Text>
             ) : (
               <Link href={item.path} style={{ textDecoration: 'none' }}>
-                <Flex align="center" color="blue.700">
-                  <Text
-                    _hover={{ color: 'blue.800', textDecoration: 'underline' }}
-                    cursor="pointer"
-                    fontWeight="bold"
-                  >
-                    {item.label}
-                  </Text>
-                </Flex>
+                <Text
+                  color="#4A90E2"
+                  fontWeight="bold"
+                  fontSize="sm"
+                  _hover={{ textDecoration: 'underline' }}
+                  cursor="pointer"
+                >
+                  {capitalizeFirstLetter(item.label)}
+                </Text>
               </Link>
             )}
-            {index < breadcrumbs.length - 1 && <IoChevronForward size={14} color="gray" />}
+            {index < breadcrumbs.length - 1 && <IoChevronForward size={12} color="#666" />}
           </Flex>
         ))}
       </Flex>
